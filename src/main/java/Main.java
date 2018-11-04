22e65905b2dd1fc513efb48549ca75d8e7e7ce6e
beyond-godlike <dia_the_ghost@mail.ru>
2018-11-04 10:35:33 +0200

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,15 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // to gen a readme of my friend`s project
-        OkHttpUtil get = new OkHttpUtil();
-        String response = get.run("https://raw.github.com/Ice-crusher/MyGmail/master/README.md");
+        OkHttpUtil util = new OkHttpUtil();
+
+        // to get a readme of my friend`s project
+        String response = util.run("https://raw.github.com/Ice-crusher/MyGmail/master/README.md");
         System.out.println(response);
 
         // to post json to a server
-        OkHttpUtil send = new OkHttpUtil();
-        String json = send.bowlingJson("Mary", "Katy");
-        String resp = send.post("http://www.roundsapp.com/post", json);
+        String json = util.bowlingJson("Mary", "Katy");
+        String resp = util.post("http://www.roundsapp.com/post", json);
         System.out.println(resp);
     }
 }
